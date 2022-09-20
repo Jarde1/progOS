76a6815b5983c6406f7a375328a9e14271077faf
Jarde1 <jardelsk77@yahoo.com>
2022-09-20 02:29:42 -0300

    alteração nos parametros de conexão com o banco de dados sql para se comunicar com o bsnco Debian Linux

--- src/br/com/infox/dal/ModuloConexao.java
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -44,11 +44,11 @@ public class ModuloConexao {
         String driver = "com.mysql.cj.jdbc.Driver";
         // armazenando infoemações referentes ao banco
         //String url = "jdbc:mysql://localhost:3306/dbinfox";
-        String url = "jdbc:mysql://localhost:3306/dbinfox?characterEncoding=utf-8";
+        String url = "jdbc:mysql://192.168.1.2:3306/dbinfox?characterEncoding=utf-8";
         //String user = "root";
         String user = "dba";
         //String password = "3016127710";
-        String password = "infox@123456";
+        String password = "Dba@123456";
         // estabelecento a conexão com o banco
         try {
 
